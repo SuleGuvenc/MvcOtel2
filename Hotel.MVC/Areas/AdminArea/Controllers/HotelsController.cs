6a78b7de2768511f886f5d2835a70cfd33b30588
SuleGuvenc <Sulenrguvenc@gmail.com>
2023-10-02 13:16:20 +0300

    yt

---
+++ Hotel.MVC/Areas/AdminArea/Controllers/HotelsController.cs
@@ -0,0 +1,161 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Otel.DAL.Contents;
+using Otel.Entitiy.Concrete;
+
+namespace Hotel.MVC.Areas.AdminArea.Controllers
+{
+    [Area("AdminArea")]
+    [Authorize(Roles = "Admin")]
+    public class HotelsController : Controller
+    {
+        private readonly SqlDbContext _context;
+
+        public HotelsController(SqlDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: AdminArea/Hotels
+        public async Task<IActionResult> Index()
+        {
+              return _context.Hotels != null ? 
+                          View(await _context.Hotels.ToListAsync()) :
+                          Problem("Entity set 'SqlDbContext.Hotels'  is null.");
+        }
+
+        // GET: AdminArea/Hotels/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Hotels == null)
+            {
+                return NotFound();
+            }
+
+            var hotel = await _context.Hotels
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (hotel == null)
+            {
+                return NotFound();
+            }
+
+            return View(hotel);
+        }
+
+        // GET: AdminArea/Hotels/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: AdminArea/Hotels/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Adres,PostaCode,HotelName,HotelCity,HotelCountry,Gsm,Id,CreateDate")] Otel.Entitiy.Concrete.Hotel hotel)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(hotel);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(hotel);
+        }
+
+        // GET: AdminArea/Hotels/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Hotels == null)
+            {
+                return NotFound();
+            }
+
+            var hotel = await _context.Hotels.FindAsync(id);
+            if (hotel == null)
+            {
+                return NotFound();
+            }
+            return View(hotel);
+        }
+
+        // POST: AdminArea/Hotels/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Adres,PostaCode,HotelName,HotelCity,HotelCountry,Gsm,Id,CreateDate")] Otel.Entitiy.Concrete.Hotel hotel)
+        {
+            if (id != hotel.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(hotel);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!HotelExists(hotel.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(hotel);
+        }
+
+        // GET: AdminArea/Hotels/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Hotels == null)
+            {
+                return NotFound();
+            }
+
+            var hotel = await _context.Hotels
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (hotel == null)
+            {
+                return NotFound();
+            }
+
+            return View(hotel);
+        }
+
+        // POST: AdminArea/Hotels/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Hotels == null)
+            {
+                return Problem("Entity set 'SqlDbContext.Hotels'  is null.");
+            }
+            var hotel = await _context.Hotels.FindAsync(id);
+            if (hotel != null)
+            {
+                _context.Hotels.Remove(hotel);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool HotelExists(int id)
+        {
+          return (_context.Hotels?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
